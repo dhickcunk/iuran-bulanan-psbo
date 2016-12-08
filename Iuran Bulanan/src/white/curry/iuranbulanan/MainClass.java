4961478402216ede744628a59057cfe135e75264
dhickcunk <dhickcunk@gmail.com>
2016-12-08 12:09:46 +0700

    Adding User Interface 20 % -_-

--- Iuran Bulanan/src/white/curry/iuranbulanan/MainClass.java
+++ Iuran Bulanan/src/white/curry/iuranbulanan/MainClass.java
@@ -5,10 +5,33 @@
  */
 package white.curry.iuranbulanan;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import white.curry.iuranbulanan.view.Main;
+
 /**
  *
  * @author Dicky Gusthia Suhada
  */
 public class MainClass {
-    
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");
+                    new Main().setVisible(true);
+                } catch (UnsupportedLookAndFeelException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InstantiationException ex) {
+                    Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
 }
